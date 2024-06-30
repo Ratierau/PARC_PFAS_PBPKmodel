7b5d4e016e8a8da051fc427ee053c4d39670f054
jirikalina <kalina@mail.muni.cz>
2024-06-30 19:39:50 +0200

    Food PFASs concentrations - format description

---
+++ Codes_PFAS_models/Kalina_Sapunova_2024_oral_exposure/food_concentrations_description.R
@@ -0,0 +1,23 @@
+#########################################################################################################################
+#                                                                                                                       #
+#  Data set of PFAS occurrence data in food and drinking water across Europe: data_pfas                                 #
+#                                                                                                                       #
+#  Variables:                                                                                                           #
+#  expoHierarchyCode.2        expo hierarchy code EFSA Level 2                                                          #
+#  expoHierarchyCode.x        expo hierarchy code EFSA at the finest possible level of detail                           #
+#  food_description           names of expo hierarchy codes EFSA Level 2                                                #
+#  reference                  reference to an article/report                                                            #
+#  chemical                   PFAS compound code                                                                        #
+#  unit                       measurement unit of PFAS levels                                                           #
+#  sample_number              number of samples                                                                         #
+#  pooled                     note if samples were pooled or not                                                        #
+#  food_origin                place where samples were purchased/sampled/imported from                                  #
+#  food_origin_specify        more specific place where samples were purchased/sampled/imported from                    #
+#  production_method          production method of samples                                                              # 
+#  date_midpoint              midpoint of a sample period                                                               #
+#  Europe_region              European region that the "country" belongs to                                             #
+#  median                     median PFAS value                                                                         #
+#  country                    country of purchase/sampling                                                              #
+#                                                                                                                       #
+#########################################################################################################################
+
