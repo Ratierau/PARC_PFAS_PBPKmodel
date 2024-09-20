560ec56efb28db1fe15466a2a51991058f3b0921
jirikalina <kalina@mail.muni.cz>
2024-09-20 13:31:02 +0200

    Add files via upload
    Fixed problem of multiple portion sizes for one food item.

--- Codes_PFAS_models/Kalina_Sapunova_2024_oral_exposure/Code/exposure.freq.R
+++ Codes_PFAS_models/Kalina_Sapunova_2024_oral_exposure/Code/exposure.freq.R
@@ -3,9 +3,11 @@
 #  Function for individual exposure including individual food frequencies: exposure.freq()                                                                                                        #
 #                                                                                                                                                                                                 #
 #  Arguments:                                                                                                                                                                                     #
-#  food.freq.individual   data.frame with individual weekly food frequencies (columns "expoHierarchyCode.x", "expoHierarchyCode.2", "freq", "region", "country")                                  #
+#  food.freq.individual   data.frame with individual weekly food frequencies (columns "expoHierarchyCode.x", "expoHierarchyCode.2", "freq")                                                       #
 #  portions               data.frame with portion sizes in grams in form of log-normal distributions (columns "expoHierarchyCode.x", "expoHierarchyCode.2", "gmean", "gsd", "region", "country")  #
 #  pfass                  vector of pfas compound codes as in the data data.frame                                                                                                                 #
+#  region                 one of "Northern Europe", "Western Europe", "Eastern Europe" or "Southern Europe"                                                                                       #
+#  country                ISO 3166 alpha-2 code                                                                                                                                                   #
 #  year                   if given, uses trend (+ CI) for estimate exposure in that year                                                                                                          #
 #  start                  lower limit of pfas data used for the estimation                                                                                                                        #
 #  end                    upper limit of pfas data used for the estimation                                                                                                                        #
@@ -20,13 +22,38 @@
 # This function uses previously defined function exposure() to compute PFASs' exposure for one individual with known food frequencies.
 # It uses exposure() for handling food PFASs' concentrations, portions.rds for a distribution of a typical portion size and individual food frequencies on form of a data frame
 
-exposure.freq<-function(food.freq.individual,portions,pfass,year=NA,start=-Inf,end=Inf,fit="moments",method="numeric",q=c(0.05,0.25,0.50,0.75,0.95),n=10000,data=data_pfas,enable.regions=TRUE) {
+exposure.freq<-function(food.freq.individual,portions,pfass,region=NA,country=NA,year=NA,start=-Inf,end=Inf,fit="moments",method="numeric",q=c(0.05,0.25,0.50,0.75,0.95),n=10000,data=data_pfas,enable.regions=TRUE) {
   
+  # Selects only portions for specified region and country.
+  if(is.na(country)) {
+    if(is.na(region)) {
+      # aggregating portions over the whole dataset
+      portions<-data.frame(      aggregate(list("gmean"=portions$gmean),by=list("expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE),
+                           "gsd"=aggregate(list("gsd"  =portions$gsd  ),by=list("expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE)$gsd)
+      rownames(portions)<-portions$expoHierarchyCode.x
+    }
+    else {
+      # aggregating portions according to region
+      portions<-portions[which(portions$region==region),]     
+      portions<-data.frame(      aggregate(list("gmean"=portions$gmean),by=list("region"=portions$region,"expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE),
+                           "gsd"=aggregate(list("gsd"  =portions$gsd  ),by=list("region"=portions$region,"expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE)$gsd)
+      rownames(portions)<-portions$expoHierarchyCode.x
+    }
+  } else {
+    # aggregating portions according to country
+    portions<-portions[which(portions$country==country),]
+    portions<-data.frame(      aggregate(list("gmean"=portions$gmean),by=list("country"=portions$country,"expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE),
+                         "gsd"=aggregate(list("gsd"  =portions$gsd  ),by=list("country"=portions$country,"expoHierarchyCode.x"=portions$expoHierarchyCode.x,"expoHierarchyCode.2"=portions$expoHierarchyCode.2),FUN="median",na.rm=TRUE)$gsd)
+    rownames(portions)<-portions$expoHierarchyCode.x
+  }
+  
+  # prepare empty output variables
   warn<-character(0)
   results<-data.frame()
   
+  # food frequency number of lines
   ffnl<-nrow(food.freq.individual)
-  food.freq.individual<-aggregate(list("freq"=food.freq.individual$freq),by=list("expoHierarchyCode.x"=food.freq.individual$expoHierarchyCode.x,"expoHierarchyCode.2"=food.freq.individual$expoHierarchyCode.2,"country"=food.freq.individual$country,"region"=food.freq.individual$region),FUN="sum")
+  food.freq.individual<-aggregate(list("freq"=food.freq.individual$freq),by=list("expoHierarchyCode.x"=food.freq.individual$expoHierarchyCode.x,"expoHierarchyCode.2"=food.freq.individual$expoHierarchyCode.2),FUN="sum")
   if(ffnl!=nrow(food.freq.individual)) {
     warn<-c(warn,"In food.freq.individual, multiple rows for same food items were aggregated.")
   }
@@ -35,7 +62,7 @@ exposure.freq<-function(food.freq.individual,portions,pfass,year=NA,start=-Inf,e
     
     contrib<-data.frame("gmean"=numeric(0),"gsd"=numeric(0),weight=numeric(0))
     for(i in 1:nrow(food.freq.individual)) {
-      contrib<-rbind(contrib,c(exposure(food.freq.individual$expoHierarchyCode.x[i],food.freq.individual$expoHierarchyCode.2[i],pfas,region=food.freq.individual$region[i],country=food.freq.individual$country[i],year=year,fit=fit,method="analytic",data=data,enable.regions=enable.regions),"weight"=food.freq.individual$freq[i]))
+      contrib<-rbind(contrib,c(exposure(food.freq.individual$expoHierarchyCode.x[i],food.freq.individual$expoHierarchyCode.2[i],pfas,region=region,country=country,year=year,fit=fit,method="analytic",data=data,enable.regions=enable.regions),"weight"=food.freq.individual$freq[i]))
     }
     rownames(contrib)<-food.freq.individual$expoHierarchyCode.x
     colnames(contrib)<-c("gmean","gsd","weight")
